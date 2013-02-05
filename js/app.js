7e7333f6da88a5e8a81236e18ef16a03776d17e0
Marius Butuc <marius.butuc@gmail.com>
2013-02-05 13:57:06 -0500

    Debugging
    [53:35]
    ✓ App.Router.router.recognizer.names
    ✓ blank state / blank slate template
    ✓ debugger;
    ✓ {{ controller }}

--- js/app.js
+++ js/app.js
@@ -20,11 +20,12 @@ App.TablesRoute = Ember.Route.extend({
 });
 
 // AUTO-GENERATED
-// App.TableRoute = Ember.Route.extend({
-//   model: function(params) {
-//     return App.Table.find(params.table_id);
-//   }
-// });
+App.TableRoute = Ember.Route.extend({
+  model: function(params) {
+    // debugger;
+    return App.Table.find(params.table_id);
+  }
+});
 
 App.TablesController = Ember.ArrayController.extend();
 
